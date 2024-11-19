e8055cc1f0b2596e0028d6c90cc425df2f7811a1
pedrinpz <159180865+pedrinpz@users.noreply.github.com>
2024-11-19 09:11:06 -0300

    Delete ATIVIDADE13/AT13-ManyToOne directory

--- ATIVIDADE13/AT13-ManyToOne/src/main/java/com/senaidev/mercado/repositories/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.senaidev.mercado.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.senaidev.mercado.entities.Categoria;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
-
-}
--- ATIVIDADE13/AT13-ManyToOne/src/main/java/com/senaidev/mercado/repositories/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.senaidev.mercado.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.senaidev.mercado.entities.Produto;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long>{
-
-}
